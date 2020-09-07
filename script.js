a63b8be6286caa17eb1a81b3fbdf7a195f254070
Ike Flo <isaac.florell@gmail.com>
2020-09-06 20:27:31 -0700

    java introduced, initial city search functions

---
+++ script.js
@@ -0,0 +1,17 @@
+const apiKey = "0765d126b0f6a7eb158764d733ae5823";
+var currWeatherDiv = $("#currentWeather");
+var forecastDiv = $("#weatherForecast");
+var citiesArray;
+if (localStorage.getItem("localWeatherSearches")) {
+    citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
+    writeSearchHistory(citiesArray);
+} else {
+    citiesArray = [];
+};
+function returnCurrentWeather(cityName) {
+    let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+    $.get(queryURL).then(function(response){
+        let currTime = new Date(response.dt*1000);
+        let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
+
+        currWeatherDiv.html(`
