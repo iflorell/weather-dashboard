28772c7e5cbc6a0b82247c25810297d56a27d734
Ike Flo <isaac.florell@gmail.com>
2020-09-06 20:52:30 -0700

    JS improvement, adjustmentg to API code typo

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-const apiKey = "0765d126b0f6a7eb158764d733ae5823";
+const apiKey = "74a4487873cba8a99f2ca1a93c2b5817";
 var currWeatherDiv = $("#currentWeather");
 var forecastDiv = $("#weatherForecast");
 var citiesArray;
+
 if (localStorage.getItem("localWeatherSearches")) {
     citiesArray = JSON.parse(localStorage.getItem("localWeatherSearches"));
     writeSearchHistory(citiesArray);
 } else {
     citiesArray = [];
 };
+
+
 function returnCurrentWeather(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&APPID=${apiKey}`;
+
     $.get(queryURL).then(function(response){
         let currTime = new Date(response.dt*1000);
         let weatherIcon = `https://openweathermap.org/img/wn/${response.weather[0].icon}@2x.png`;
@@ -23,5 +27,32 @@ function returnCurrentWeather(cityName) {
         createHistoryButton(response.name);
     })
 };
+
 function returnWeatherForecast(cityName) {
     let queryURL = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=metric&APPID=${apiKey}`;
+
+    $.get(queryURL).then(function(response){
+        let forecastInfo = response.list;
+        forecastDiv.empty();
+        $.each(forecastInfo, function(i) {
+            if (!forecastInfo[i].dt_txt.includes("12:00:00")) {
+                return;
+            }
+            let forecastDate = new Date(forecastInfo[i].dt*1000);
+            let weatherIcon = `https://openweathermap.org/img/wn/${forecastInfo[i].weather[0].icon}.png`;
+            forecastDiv.append(`
+            <div class="col-md">
+                <div class="card text-white bg-primary">
+                    <div class="card-body">
+                        <h4>${forecastDate.getMonth()}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <h4>${forecastDate.getMonth()+1}/${forecastDate.getDate()}/${forecastDate.getFullYear()}</h4>
+                        <img src=${weatherIcon} alt="Icon">
+                        <p>Temp: ${forecastInfo[i].main.temp} &#176;C</p>
+                        <p>Humidity: ${forecastInfo[i].main.humidity}%</p>
+                    </div>
+                </div>
+            </div>
+            `)
+        })
+    })
+};
